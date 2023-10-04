6d0435f596a4b8d811b4bbb7d5bd94d614265581
Ayrat Hudaygulov <szer.penguin@gmail.com>
2023-10-04 21:00:31 +0100

    pluralize

--- src/VahterBanBot/Utils.fs
+++ src/VahterBanBot/Utils.fs
@@ -28,13 +28,19 @@ let prependUsername (s: string) =
         s
     else "@" + s
 
+let pluralize (n: float) (s: string) =
+    if n < 2.0 then
+        ""
+    else
+        "s"
+
 let timeSpanAsHumanReadable (ts: TimeSpan) =
     let totalSeconds = ts.TotalSeconds
     if totalSeconds < 60.0 then
-        $"%.0f{totalSeconds} seconds"
+        $"%.0f{totalSeconds} second{pluralize totalSeconds}"
     elif totalSeconds < 3600.0 then
-        $"%.0f{ts.TotalMinutes} minutes"
+        $"%.0f{ts.TotalMinutes} minute{pluralize ts.TotalMinutes}"
     elif totalSeconds < 86400.0 then
-        $"%.0f{ts.TotalHours} hours"
+        $"%.0f{ts.TotalHours} hour{pluralize ts.TotalHours}"
     else
-        $"%.0f{ts.TotalDays} days"
+        $"%.0f{ts.TotalDays} day{pluralize ts.TotalDays}"
