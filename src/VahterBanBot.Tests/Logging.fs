dfa46b2b95f405e8703575682ef576286e09f449
Friedrich von Never <friedrich@fornever.me>
2024-10-23 13:07:24 +0300

    More robust testing (#62)
    Tests: more robust logging for failed containers, destroy test container before startup

---
+++ src/VahterBanBot.Tests/Logging.fs
@@ -0,0 +1,16 @@
+﻿namespace VahterBanBot.Tests
+
+open System
+open Microsoft.Extensions.Logging
+
+type StringLogger() =
+    let lockObj = obj()
+    let messages = ResizeArray<string>() 
+    interface ILogger with
+        member this.BeginScope _ = null
+        member this.IsEnabled _ = true
+        member this.Log(logLevel, _eventId, state, ex, formatter) =
+            lock lockObj (fun() ->
+                messages.Add($"[{logLevel}] {formatter.Invoke(state, ex)}"))
+
+    member _.ExtractMessages(): string = lock lockObj (fun() -> String.Join("\n", messages))
